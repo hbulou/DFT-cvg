97ad5c37621f8829017e6a08747055069ba642b4
hbulou <herve.bulou@ipcms.unistra.fr>
2025-01-10 09:22:32 +0100

    README.md màj

--- scripts/PP.py
+++ scripts/PP.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-###!/home/bulou/virtual/bin/python3
-
+#!/home/bulou/virtual/bin/python3
+#####!/usr/bin/python3
 
 import numpy
 import pandas
@@ -10,43 +9,46 @@ import argparse
 def read_data(filename):
     head=pandas.read_csv(filename,nrows=1,header=None)
     entete=head.values[0][0].split()[1:]
-    print(entete,len(entete))
-    for i in range(1,len(entete)):
-        if entete[i] in entete[:i-1]:
-            entete[i]=entete[i]+str(entete[:i-1].count(entete[i]))
-    print(entete,len(entete))
-
+    print(entete)
     data=pandas.read_csv(filename,header=0,index_col=False,names=entete, delim_whitespace=True)  #sep=None) #,skiprows=[0],header=None,names=entete)#,header=None,name=[entete])
     print(data)
     return data
-def main(args):
-
-    data=read_data("ld1.wfc")
-    dataps=read_data("ld1ps.wfc")
-    fig, ax = pyplot.subplots()
 
 
-    print(args.idx)
-    if len(args.idx)>0:
-        for tag in args.idx:
+def read_file(datafile,args,fig,ax):
+
+    
+    data=read_data(datafile)
+    #data=read_data("ld1ps.wfc")
+
+
+
+    print(args.list)
+    if len(args.list)>0:
+        for tag in args.list:
             ax.plot(data.r,data[tag],label=tag)
-            for d in dataps.columns:
-                if tag in d:
-                    ax.plot(dataps.r,dataps[d],label=d)
-                    ax.plot(dataps.r,-dataps[d],label=d)
-            
-    ax.legend()
-    for r in args.rcut:
-        print(r)
-        pyplot.axvline(x=float(r))
-    pyplot.xlim(0, args.xmax)
-    #plt.ylim(-1.5, 1.5)
-    pyplot.show()
+            #ax.plot(dataps.r,dataps[tag])
+
+def main(args):
+    if args.files is not None:
+        fig, ax = pyplot.subplots()
+        for datafile in args.files:
+            read_file(datafile,args,fig,ax)
+        
+        ax.legend()
+        if args.rcut is not None:
+            for r in args.rcut:
+                print(r)
+                pyplot.axvline(x=float(r))
+
+        pyplot.xlim(0, args.xmax)
+        pyplot.show()
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--idx'   ,nargs='+')  # nargs='*' if you want to support the ability to have an empty list.
+    parser.add_argument('--files'   ,nargs='+')  # nargs='*' if you want to support the ability to have an empty list.
+    parser.add_argument('--list'   ,nargs='+')  # nargs='*' if you want to support the ability to have an empty list.
+    parser.add_argument('--xmax'   ,nargs='?',type=float,default=20.0)
     parser.add_argument('--rcut'   ,nargs='+')
-    parser.add_argument('--xmax'   ,nargs='?',type=float,default=20.0)  
     args = parser.parse_args()
     main(args)
 
---
+++ scripts/gen_Rh_PP.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#SBATCH --account=pcm7459
+#SBATCH --constraint=GENOA
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=0:30:00
+
+
+
+ARGUMENT_LIST=(
+    "h"
+    "pp_filename"
+    ""
+)
+# read arguments
+opts=$(getopt \
+  --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+  --name "$(basename "$0")" \
+  --options "" \
+  -- "$@"
+)
+eval set --$opts
+
+
+fct='PBE'
+pp_filename="Rh.${fct}-spn-kjpaw_psl.0.3.0.UPF"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+	--h) # display Help
+            Help
+	    exit
+            #shift 2
+	    ;;
+	--pp_filename)
+	    pp_filename=$2
+            shift 2
+	    ;;
+	*)
+	    break;;
+    esac
+done
+
+
+elt='Rh'
+Z=45.
+nrel=1
+
+INPUTFILE="gen_PP.in"
+OUTPUTFILE="gen_PP.out"
+
+cat > $INPUTFILE <<EOF
+ &input
+   title='${elt}',
+   zed=$Z,
+   rel=$nrel,
+   config='[Kr] 4d8. 5s1.0 5p0.0',
+   iswitch=3,
+   dft='$fct'
+ /
+ &inputp
+   lpaw=.true.,
+   pseudotype=3,
+   file_pseudopw='${pp_filename}',
+   author='HB',
+   lloc=-1,
+   rcloc=1.9,
+   which_augfun='PSQ',
+   rmatch_augfun_nc=.true.,
+   nlcc=.true.,
+   new_core_ps=.true.,
+   rcore=0.9,
+   tm=.true.
+ /
+6
+4S  1  0  2.00  0.00  0.90  1.70  0.0
+5S  2  0  1.00  0.00  0.90  1.30  0.0
+4P  2  1  6.00  0.00  0.90  1.70  0.0
+5P  3  1  0.00  0.00  0.90  1.70  0.0
+4D  3  2  8.00  0.00  0.90  1.90  0.0
+4D  3  2 -2.00  0.30  0.90  1.90  0.0
+EOF
+
+
+CMD="ld1.x"
+if [ $(hostname) == 'pc-hervecal' ] ; then
+    echo "this machine is pc-hervecal"
+    /home/bulou/ownCloud/src/QE/q-e-develop/bin/$CMD < $INPUTFILE  | tee $OUTPUTFILE
+else
+    module purge
+
+    module load develop GCC-CPU-3.1.0
+    module load quantum-espresso
+
+    module list
+
+    # export OMP_DISPLAY_AFFINITY=TRUE
+    export OMP_PROC_BIND=CLOSE
+    export OMP_PLACES=THREADS
+
+    export OMP_NUM_THREADS=1
+
+
+
+
+
+    # MI250 or GENOA or HPDA
+    #salloc --account=pcm7459 --constraint=GENOA --job-name="gen_PP" --nodes=1 --time=1:00:00 --exclusive
+    srun --ntasks-per-node=1 --cpus-per-task=1 --threads-per-core=1 --label  --  $CMD < $INPUTFILE  | tee $OUTPUTFILE
+    #srun --ntasks-per-node=1 --cpus-per-task="${OMP_NUM_THREADS}" --threads-per-core=1 --label  -- $CMD < ${RUNNAME}.in | tee ${RUNNAME}.out
+    
+fi
+
+
+
+
+
+
+
